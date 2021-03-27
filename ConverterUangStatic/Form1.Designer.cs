1ea618a602771448905a0efb7b52d1aae490fd6a
Afif Fadhlurrahman <afif.fadhlurrahman1@gmail.com>
2021-03-27 12:36:34 +0700

    Add files via upload

---
+++ ConverterUangStatic/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ConverterUangStatic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Europe - Euro",
+            "Malaysia - Ringgit",
+            "United State - Dollar",
+            "Kuwait - Dinar",
+            ""});
+            this.comboBox1.Location = new System.Drawing.Point(166, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(221, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Value to Convert";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "To";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Europe - Euro",
+            "Malaysia - Ringgit",
+            "United State - Dollar",
+            "Kuwait - Dinar",
+            ""});
+            this.comboBox2.Location = new System.Drawing.Point(166, 125);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(221, 21);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 205);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(221, 55);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Answer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 303);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Currency Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ConverterUangStatic/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO;
+
+namespace ConverterUangStatic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try{
+
+                double valCurr = double.Parse(textBox1.Text);
+                string fromVal = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                string toVal = comboBox2.Items[comboBox2.SelectedIndex].ToString();
+                //if()
+                double hasil = 0;
+                if(comboBox1.SelectedIndex == 0)
+                {
+                    // MessageBox.Show(fromVal);
+                    // Euro
+                    if (comboBox2.SelectedIndex == 0) hasil = valCurr;
+                    else if (comboBox2.SelectedIndex == 1) hasil = valCurr * 4.89;
+                    else if (comboBox2.SelectedIndex == 2) hasil = valCurr * 1.18;
+                    else if (comboBox2.SelectedIndex == 3) hasil = valCurr * 0.356;
+                }
+                else if(comboBox1.SelectedIndex == 1)
+                {
+                    // MessageBox.Show(fromVal);
+                    // Malaysia - Ringgit
+                    if (comboBox2.SelectedIndex == 0) hasil = valCurr * 0.21;
+                    else if (comboBox2.SelectedIndex == 1) hasil = valCurr;
+                    else if (comboBox2.SelectedIndex == 2) hasil = valCurr * 0.24;
+                    else if (comboBox2.SelectedIndex == 3) hasil = valCurr * 0.07;
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    // MessageBox.Show(fromVal);
+                    // United State - Dollar
+                    if (comboBox2.SelectedIndex == 0) hasil = valCurr * 0.8475;
+                    else if (comboBox2.SelectedIndex == 1) hasil = valCurr * 4.14;
+                    else if (comboBox2.SelectedIndex == 2) hasil = valCurr;
+                    else if (comboBox2.SelectedIndex == 3) hasil = valCurr * 0.30;
+                }
+                else if (comboBox1.SelectedIndex == 3)
+                {
+                    // MessageBox.Show(fromVal);
+                    // Kuwait - Dinar
+                    if (comboBox2.SelectedIndex == 0) hasil = valCurr * 2.81;
+                    else if (comboBox2.SelectedIndex == 1) hasil = valCurr * 13.72;
+                    else if (comboBox2.SelectedIndex == 2) hasil = valCurr * 3.31;
+                    else if (comboBox2.SelectedIndex == 3) hasil = valCurr;
+                }
+                else
+                {
+                    MessageBox.Show("GAADA");
+                }
+                //MessageBox.Show("from : " + fromVal + " to : " + toVal + " " + valCurr);
+                MessageBox.Show("HASIL : " + hasil);
+                textBox2.Text = hasil.ToString();
+
+            }
+            catch (FormatException ex){
+                MessageBox.Show("Nilai yang dimasukkan harus integer");
+            }
+            catch (ArgumentOutOfRangeException ex){
+                MessageBox.Show("Currency from harus dipilih");
+            }
+            catch (Exception ex){
+                MessageBox.Show("Error : " + ex);
+            }
+        }
+    }
+}
